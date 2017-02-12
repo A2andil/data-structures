d745da2a1e727f433fa2cff27f936ff6457f42dc
Ahmad Kandil <kandileng@gmail.com>
2017-02-12 09:30:54 +0200

    Heap

--- 5- Binary Min Heap.cpp
+++ 5- Binary Min Heap.cpp
@@ -1,27 +1,21 @@
-//in the name of God
+//in theape name of God
 
 #include <iostream>
 #include <limits>
 
+#define ll long long
 using namespace std;
 
-void swap(int *x, int *y);
-
-class MinHeap {
+class Minheapeap {
 private:
-        int *list;
-        int capacity;
-        int size;
+        int *list, capacity, size;
 public:
-        MinHeap(int capacity);
-        void MinHeapify(int);
-        int parent(int i) {
-                return (i - 1) / 2;
-        }
+        Minheapeap(int capacity);
+        void Minheapeapify(int);
         int left(int i) {
                 return 2 * i + 1;
         }
-        int right(int i) {
+        int righeapt(int i) {
                 return 2 * i + 2;
         }
         int extractMin();
@@ -33,13 +27,13 @@ public:
         void insertKey(int k);
 };
 
-MinHeap::MinHeap(int sz) {
+Minheapeap::Minheapeap(int sz) {
         size = 0;
         capacity = sz;
         list = new int[sz];
 }
 
-void MinHeap::insertKey(int k) {
+void Minheapeap::insertKey(int k) {
         if (size == capacity) {
                 cout << "\nOverflow: Could not insertKey\n";
                 return;
@@ -47,21 +41,21 @@ void MinHeap::insertKey(int k) {
         size = size + 1;
         int i = size - 1;
         list[i] = k;
-        while (i != 0 && list[parent(i)] > list[i]) {
-                swap(&list[i], &list[parent(i)]);
-                i = parent(i);
+        while (i != 0 && list[(i - 1) / 2] > list[i]) {
+                swap(list[i], list[(i - 1) / 2]);
+                i = (i - 1) / 2;
         }
 }
 
-void MinHeap::decreaseKey(int i, int val) {
+void Minheapeap::decreaseKey(int i, int val) {
         list[i] = val;
-        while (i != 0 && list[parent(i)] > list[i]) {
-                swap(&list[i], &list[parent(i)]);
-                i = parent(i);
+        while (i != 0 && list[(i - 1) / 2] > list[i]) {
+                swap(list[i], list[(i - 1) / 2]);
+                i = (i - 1) / 2;
         }
 }
 
-int MinHeap::extractMin() {
+int Minheapeap::extractMin() {
         if (size <= 0)
                 return INT_MAX;
         if (size == 1) {
@@ -70,48 +64,42 @@ int MinHeap::extractMin() {
         }
         int root = list[0];
         list[0] = list[size - 1];
-        size--;
-        MinHeapify(0);
+        size = size - 1;
+        Minheapeapify(0);
         return root;
 }
 
-void MinHeap::deleteKey(int i) {
+void Minheapeap::deleteKey(int i) {
         decreaseKey(i, INT_MIN);
         extractMin();
 }
 
-void MinHeap::MinHeapify(int i) {
-        int l = left(i);
-        int r = right(i);
+void Minheapeap::Minheapeapify(int i) {
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
         int smallest = i;
         if (l < size && list[l] < list[i])
                 smallest = l;
         if (r < size && list[r] < list[smallest])
                 smallest = r;
         if (smallest != i) {
-                swap(&list[i], &list[smallest]);
-                MinHeapify(smallest);
+                swap(list[i], list[smallest]);
+                Minheapeapify(smallest);
         }
 }
 
-void swap(int *x, int *y) {
-        int temp = *x;
-        *x = *y;
-        *y = temp;
-}
-
 int main() {
-        MinHeap h(11);
-        h.insertKey(3);
-        h.insertKey(2);
-        h.deleteKey(1);
-        h.insertKey(15);
-        h.insertKey(5);
-        h.insertKey(4);
-        h.insertKey(45);
-        cout << h.extractMin() << endl;
-        cout << h.getMin() << endl;
-        h.decreaseKey(2, 1);
-        cout << h.getMin() << endl;
+        Minheapeap heap(11);
+        heap.insertKey(3);
+        heap.insertKey(2);
+        heap.deleteKey(1);
+        heap.insertKey(15);
+        heap.insertKey(5);
+        heap.insertKey(4);
+        heap.insertKey(45);
+        cout << heap.extractMin() << endl;
+        cout << heap.getMin() << endl;
+        heap.decreaseKey(2, 1);
+        cout << heap.getMin() << endl;
         return 0;
 }
